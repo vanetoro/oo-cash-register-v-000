bb43a3797e6ca0c4dde6e250d2bdf8b1501b17b2
Vanessa Toro <vanetoro84@gmail.com>
2018-03-31 20:37:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
    def apply_discount
      @total= @total - (@discount * 10)
      @total
-     puts "After the discount, the total comes to #{@total}."
+     "After the discount, the total comes to #{@total}."
    end
   
 end 
