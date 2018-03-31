48d2c72d5c0c3aa9774278fa90241e5f6402253d
Vanessa Toro <vanetoro84@gmail.com>
2018-03-31 20:48:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ require 'pry'
 class CashRegister
    attr_accessor :total, :discount
    
-   @items =[ ]
+   items =[ ]
    
    def initialize(discount = 0)
      @total = 0
@@ -28,7 +28,7 @@ class CashRegister
    end
    
    def items
-     @items
+     items
    end
   
 end 
